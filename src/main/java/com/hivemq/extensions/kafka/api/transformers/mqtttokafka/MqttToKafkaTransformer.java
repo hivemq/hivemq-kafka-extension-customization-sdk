a51afb9092648e0e184d9491a454c95c80d64bd5
Georg <georg.held@hivemq.com>
2020-07-22 16:47:09 +0200

    Added more descriptions.

--- src/main/java/com/hivemq/extensions/kafka/api/transformers/mqtttokafka/MqttToKafkaTransformer.java
+++ src/main/java/com/hivemq/extensions/kafka/api/transformers/mqtttokafka/MqttToKafkaTransformer.java
@@ -13,16 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.extensions.kafka.api.transformers.mqtttokafka;
 
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.extensions.kafka.api.transformers.Transformer;
 
+import java.util.List;
+
 /**
+ * Implement this transformer for the programmatic creation of {@link com.hivemq.extensions.kafka.api.model.KafkaRecord}s
+ * from {@link com.hivemq.extension.sdk.api.packets.publish.PublishPacket}s.
+ * <p>
+ * Your implementation of the  MqttToKafkaTransformer must be placed in a java archive (.jar) together with all its
+ * dependencies in the {@code extensions} folder of the HiveMQ Enterprise Extension for Kafka. In addition a {@code
+ * <mqtt-to-kafka-transformer>} referencing the implementing class via its canonical name must be configured in the
+ * {@code kafka-extension.xml} file.
+ *
  * @author Christoph Schäbel
+ * @author Georg Held
+ * @since 4.4.0
  */
 public interface MqttToKafkaTransformer extends Transformer<MqttToKafkaInitInput> {
 
+    /**
+     * This callback is executed for every mqtt PUBLISH that arrives at your HiveMQ cluster matching the in the {@code
+     * <mqtt-to-kafka-transformer>} tag configured {@code <mqtt-topic-filters>}. It allows the publication of any number
+     * of {@link com.hivemq.extensions.kafka.api.model.KafkaRecord}s via the {@link MqttToKafkaOutput} object.
+     *
+     * @param input  the {@link MqttToKafkaInput} contains the triggering {@link com.hivemq.extension.sdk.api.packets.publish.PublishPacket}
+     *               and the {@link com.hivemq.extensions.kafka.api.model.KafkaCluster} information. The {@link
+     *               com.hivemq.extensions.kafka.api.services.KafkaTopicService} can be accessed.
+     * @param output pass the list of new {@link com.hivemq.extensions.kafka.api.model.KafkaRecord}s to the {@link
+     *               MqttToKafkaOutput#setKafkaRecords(List)} methods.
+     * @since 4.4.0
+     */
     void transformMqttToKafka(@NotNull MqttToKafkaInput input, @NotNull MqttToKafkaOutput output);
-
 }
