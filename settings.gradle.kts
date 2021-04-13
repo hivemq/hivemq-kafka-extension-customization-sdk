2df6bd87ad51c8a18edfae6f3b1fdc0f92bc8e53
Silvio Giebl <silvio.giebl@hivemq.com>
2021-04-13 17:28:02 +0200

    build > unification

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,11 @@ rootProject.name = "hivemq-kafka-extension-customization-sdk"
 
 pluginManagement {
     plugins {
+        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
         id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
-        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
     }
 }
 
