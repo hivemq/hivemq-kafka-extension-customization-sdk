hivemq/hivemq-amazon-kinesis-extension-customization-sdk

build > use = instead of .set()

build.gradle.kts
@@ -25,30 +25,30 @@ group = "com.hivemq"
 description = "SDK for the development of HiveMQ Amazon Kinesis Extension customizations"
 
 metadata {
-    readableName.set("HiveMQ Amazon Kinesis Extension Customization SDK")
+    readableName = "HiveMQ Amazon Kinesis Extension Customization SDK"
     organization {
-        name.set("HiveMQ GmbH")
-        url.set("https://www.hivemq.com/")
+        name = "HiveMQ GmbH"
+        url = "https://www.hivemq.com/"
     }
     license {
         apache2()
     }
     developers {
         register("hurtadosanti") {
-            fullName.set("Santiago Hurtado")
-            email.set("santiago.hurtado@hivemq.com")
+            fullName = "Santiago Hurtado"
+            email = "santiago.hurtado@hivemq.com"
         }
         register("Donnerbart") {
-            fullName.set("David Sondermann")
-            email.set("david.sondermann@hivemq.com")
+            fullName = "David Sondermann"
+            email = "david.sondermann@hivemq.com"
         }
         register("YannickWeber") {
-            fullName.set("Yannick Weber")
-            email.set("yannick.weber@hivemq.com")
+            fullName = "Yannick Weber"
+            email = "yannick.weber@hivemq.com"
         }
         register("mario-schwede-hivemq") {
-            fullName.set("Mario Schwede")
-            email.set("mario.schwede@hivemq.com")
+            fullName = "Mario Schwede"
+            email = "mario.schwede@hivemq.com"
         }
     }
     github {
@@ -69,7 +69,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion = JavaLanguageVersion.of(11)
     }
     withJavadocJar()
     withSourcesJar()

hivemq/hivemq-kafka-extension-customization-sdk

build.gradle.kts
@@ -25,34 +25,34 @@ group = "com.hivemq"
 description = "SDK for the development of HiveMQ Kafka Extension customizations"
 
 metadata {
-    readableName.set("HiveMQ Kafka Extension Customization SDK")
+    readableName = "HiveMQ Kafka Extension Customization SDK"
     organization {
-        name.set("HiveMQ GmbH")
-        url.set("https://www.hivemq.com/")
+        name = "HiveMQ GmbH"
+        url = "https://www.hivemq.com/"
     }
     license {
         apache2()
     }
     developers {
         register("cschaebe") {
-            fullName.set("Christoph Schaebel")
-            email.set("christoph.schaebel@hivemq.com")
+            fullName = "Christoph Schaebel"
+            email = "christoph.schaebel@hivemq.com"
         }
         register("lbrandl") {
-            fullName.set("Lukas Brandl")
-            email.set("lukas.brandl@hivemq.com")
+            fullName = "Lukas Brandl"
+            email = "lukas.brandl@hivemq.com"
         }
         register("flimpoeck") {
-            fullName.set("Florian Limpoeck")
-            email.set("florian.limpoeck@hivemq.com")
+            fullName = "Florian Limpoeck"
+            email = "florian.limpoeck@hivemq.com"
         }
         register("sauroter") {
-            fullName.set("Georg Held")
-            email.set("georg.held@hivemq.com")
+            fullName = "Georg Held"
+            email = "georg.held@hivemq.com"
         }
         register("SgtSilvio") {
-            fullName.set("Silvio Giebl")
-            email.set("silvio.giebl@hivemq.com")
+            fullName = "Silvio Giebl"
+            email = "silvio.giebl@hivemq.com"
         }
     }
     github {
@@ -73,7 +73,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion = JavaLanguageVersion.of(11)
     }
     withJavadocJar()
     withSourcesJar()